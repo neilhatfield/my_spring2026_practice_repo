19b439b585349464318a45d2ab5176e541eb9803
neilhatfield <phiguy1114@gmail.com>
2026-04-13 09:39:59 -0400

    Add Template Files
    This commit adds two template files. The first is the function transition document that we used to create our own documented functions in R. The second is the template file for setting up an R script for data wrangling.

---
+++ wrangling_template.R
@@ -0,0 +1,21 @@
+# Short Title ----
+## Brief description of what your goals are
+## Be sure to start each line with ##
+
+# Step 1: Load Packages ----
+## Needed Packages:
+
+# Step 2: Load Data ----
+## Needed data:
+
+# Step 3: short description ----
+
+# Step 4: short description ----
+
+## Step 4A: short description ----
+
+## Step 4B: short description ----
+
+## Repeat making steps as needed
+
+
